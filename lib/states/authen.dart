e4daaf77baaf6b54d66d7f796c4d0dc6a76a505e
Aekchai1905 <84978218+Aekchai1905@users.noreply.github.com>
2021-11-13 16:39:32 +0700

    update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -13,38 +13,123 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            buildImage(size),
-            buildAppName(),
-          ],
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+          behavior: HitTestBehavior.opaque,
+          child: ListView(
+            children: [
+              buildImage(size),
+              buildAppName(),
+              buildUser(size),
+              buildPassword(size),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              labelText: "User :",
+              prefixIcon: Icon(
+                Icons.format_color_fill,
+                color: MyConstant.dark,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.light),
+                  borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                },
+                icon: statusRedEye
+                    ? Icon(
+                        Icons.remove_red_eye,
+                        color: MyConstant.dark,
+                      )
+                    : Icon(
+                        Icons.remove_red_eye_outlined,
+                        color: MyConstant.dark,
+                      ),
+              ),
+              labelStyle: MyConstant().h3Style(),
+              labelText: "Password :",
+              prefixIcon: Icon(
+                Icons.lock_outline,
+                color: MyConstant.dark,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.light),
+                  borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Row buildAppName() {
-    return Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShowTitle(title: MyConstant.appName, textStyle: MyConstant().h1Style()),
-            ],
-          );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(title: MyConstant.appName, textStyle: MyConstant().h1Style()),
+      ],
+    );
   }
 
   Row buildImage(double size) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center ,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-            width: size * 0.6,
-            child: ShowImage(
-              pathImage: MyConstant.image4,
-            ),
+          width: size * 0.6,
+          child: ShowImage(
+            pathImage: MyConstant.image4,
           ),
+        ),
       ],
     );
   }
